6cd5c487c9532c2177c86509901394b702511241
RosHom <roshom68@gmail.com>
2017-08-15 15:28:23 +0300

    Init Messenger

---
+++ Messenger Template/Messenger Template/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Messenger_Template.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Messenger Template/Messenger Template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Messenger_Template.MvcApplication" Language="C#" %>
